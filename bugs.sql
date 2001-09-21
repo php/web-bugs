4fad63138e57d0bef034002fadfeaf8118e56e8b
jim winstead <jimw@php.net>
2001-09-21 00:59:45 +0000

    initial checkin. mostly copied, with some cleaning up, from phpweb.

---
+++ bugs.sql
@@ -0,0 +1,31 @@
+# MySQL dump 4.0
+#
+# Host: localhost    Database: php3
+#--------------------------------------------------------
+DROP table bugdb;
+
+CREATE TABLE bugdb (
+  id int(8) NOT NULL AUTO_INCREMENT,
+  bug_type char(32),
+  email varchar(40) NOT NULL,
+  sdesc varchar(80),
+  ldesc text,
+  php_version char(16),
+  php_os varchar(32),
+  status varchar(16),
+  ts1 datetime, # bug created date
+  ts2 datetime, # bug last updated date
+  dev_id varchar(16),# developer who last commented
+  assign varchar(16),
+  passwd varchar(20),# user password
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE bugdb_comments (
+  id int(8) NOT NULL AUTO_INCREMENT,
+  bug int(8) NOT NULL,
+  email varchar(40) NOT NULL,
+  ts datetime NOT NULL,
+  comment text,
+  PRIMARY KEY (id)
+);
---
+++ credits.php
@@ -0,0 +1,17 @@
+<?php
+
+commonHeader('Credits');
+
+?>
+
+<h1>Credits</h1>
+
+<P>
+Coming soon ...
+</P>
+
+<?php
+
+commonFooter();
+
+?>
