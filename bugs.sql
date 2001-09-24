94dcd32c2c7d16822a83b04ac7894637bc027b99
jim winstead <jimw@php.net>
2001-09-24 02:33:56 +0000

    update table description

--- bugs.sql
+++ bugs.sql
@@ -8,8 +8,8 @@ CREATE TABLE bugdb (
   id int(8) NOT NULL AUTO_INCREMENT,
   bug_type char(32),
   email varchar(40) NOT NULL,
-  sdesc varchar(80),
-  ldesc text,
+  sdesc varchar(80) NOT NULL,
+  ldesc text NOT NULL,
   php_version char(16),
   php_os varchar(32),
   status varchar(16),
@@ -18,7 +18,8 @@ CREATE TABLE bugdb (
   dev_id varchar(16),# developer who last commented
   assign varchar(16),
   passwd varchar(20),# user password
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FULLTEXT (email,sdesc,ldesc)
 );
 
 CREATE TABLE bugdb_comments (
@@ -26,6 +27,7 @@ CREATE TABLE bugdb_comments (
   bug int(8) NOT NULL,
   email varchar(40) NOT NULL,
   ts datetime NOT NULL,
-  comment text,
-  PRIMARY KEY (id)
+  comment text NOT NULL,
+  PRIMARY KEY (id),
+  FULLTEXT (comment)
 );
