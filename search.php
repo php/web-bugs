718d8b02373cd9b3946ca2f96ce0289ae8b72ec3
Stig Bakken <ssb@php.net>
2001-10-16 08:36:17 +0000

    * on request from Derick: don't show "Closed" and "Bogus" bugs when   searching "All" assigned bugs

--- search.php
+++ search.php
@@ -30,6 +30,8 @@ if (isset($cmd) && $cmd == "display") {
 		$where_clause .= " AND status='Feedback' AND TO_DAYS(NOW())-TO_DAYS(ts2)>60";
 	} elseif ($status == "Stale") {
 		$where_clause .= " AND status != 'Closed' AND status != 'Duplicate' AND status != 'Bogus' AND TO_DAYS(NOW())-TO_DAYS(ts2) > 30";
+	} elseif ($status == "All" && !empty($assign)) {
+		$where_clause .= " AND status != 'Closed' AND status != 'Bogus'";
 	} elseif ($status && $status != "All") {
 		$where_clause .= " AND status='$status'";
 	}
@@ -93,7 +95,7 @@ if (isset($cmd) && $cmd == "display") {
 		if (!$res) die(htmlspecialchars($query)."<br>".mysql_error());
 	    $rows = mysql_numrows($res);
 
-		$link = "$PHP_SELF?cmd=display&amp;bug_type=" . urlencode ($bug_type) . "&amp;status=$status&amp;search_for=".urlencode(htmlspecialchars(stripslashes($search_for)))."&amp;php_os=".htmlspecialchars(stripslashes($php_os))."&amp;bug_age=$bug_age&amp;by=$by&amp;order_by=$order_by&amp;direction=$direction&amp;phpver=$phpver&amp;limit=$limit";
+		$link = "$PHP_SELF?cmd=display&amp;bug_type=" . urlencode ($bug_type) . "&amp;status=$status&amp;search_for=".urlencode(htmlspecialchars(stripslashes($search_for)))."&amp;php_os=".htmlspecialchars(stripslashes($php_os))."&amp;bug_age=$bug_age&amp;by=$by&amp;order_by=$order_by&amp;direction=$direction&amp;phpver=$phpver&amp;limit=$limit&amp;assign=$assign";
 ?>
 <table align="center" border="0" cellspacing="2" width="95%">
  <?php show_prev_next($begin,$rows,$total_rows,$link,$limit);?>
