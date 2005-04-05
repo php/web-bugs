128235cae4e11356872a5b96d289e2646d194a3c
Gabor Hojtsy <goba@php.net>
2005-04-05 11:34:50 +0000

    inform people about fix variations of text

--- quick-fix-desc.php
+++ quick-fix-desc.php
@@ -6,16 +6,24 @@ commonHeader("Quick Fix Descriptions");
 ?>
 <table border="2" cellpadding="6">
 <?php
-
-	foreach ($RESOLVE_REASONS as $reason) {
-		echo "
-				<tr>
-					<td>{$reason['desc']}</td>
-					<td>Status: {$reason['status']}</td>  
-					<td><pre>{$reason['message']}</pre></td>
-				</tr>
-		";
-	} 
+foreach ($RESOLVE_REASONS as $key => $reason) {
+    echo "
+    <tr>
+     <td>{$reason['desc']}</td>
+     <td>Status: {$reason['status']}</td>  
+     <td><pre>{$reason['message']}</pre></td>
+    </tr>";
+    if (isset($FIX_VARIATIONS[$key])) {
+        foreach ($FIX_VARIATIONS[$key] as $type => $variation) {
+            echo "
+            <tr>
+             <td>{$reason['desc']} ({$type})</td>
+             <td>Status: {$reason['status']}</td>  
+             <td><pre>{$variation}</pre></td>
+            </tr>";
+        }
+    }
+} 
 ?>
 </table>    
 <?php 
