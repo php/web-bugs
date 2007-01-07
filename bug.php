d612a673710be05b8e0871fbd64476ad72ae5cd0
Hannes Magnusson <bjori@php.net>
2007-01-07 08:46:22 +0000

    Fixed bug#37728 (let users add comments on closed bugs, or at least tell him)

--- bug.php
+++ bug.php
@@ -322,9 +322,15 @@ just go ahead and add more information to this bug or edit the other fields.
 <div class="explain">
 <?php if (!$in) {?>
 Welcome back! If you're the original bug submitter, here's where you can edit
-the bug or add additional notes. If this is not your bug, you can <a
+the bug or add additional notes.
+<?php if($comments_allowed) {?>
+If this is not your bug, you can <a
 href="<?php echo "$self?id=$id&amp;edit=3"?>">add a comment by following
-this link</a> or the box above that says 'Add Comment'. If this is your bug,
+this link</a> or the box above that says 'Add Comment'.
+<?php } else {?>
+If this is not your bug, you can not add a comment to this report as it is marked as closed.
+<?php } ?>
+If this is your bug,
 but you forgot your password, <a href="bug-pwd-finder.php">you can retrieve
 your password here</a>.
 <?php }?>
