8cdf14da8fbc6f2447f9b6c884f66cde54fc7841
foobar <sniper@php.net>
2007-06-14 12:24:15 +0000

    tune resolve import to DB script (not used yet!)

--- convert_resolves_to_db.php
+++ convert_resolves_to_db.php
@@ -12,23 +12,18 @@ $i = 0;
 if ($res)
 	while ($row = mysql_fetch_row($res)) $i++;
 
-if ($i == 0)
+foreach ($RESOLVE_REASONS as $key => $r)
 {
-	foreach ($RESOLVE_REASONS as $key => $r)
-	{
-		$status = mysql_escape_string($r['status']);
-		$title = mysql_escape_string($r['desc']);
-		$message = mysql_escape_string($r['message']);
-		mysql_query("
-			INSERT INTO bugdb_resolves
-			SET status = '$status',
-				title = '$title',
-				message = '$message'
-		");
-		echo mysql_error();
-	}
-}
-else
-{
-	echo "bugdb_resolves already populated!\n";
+	$key = mysql_escape_string($key);
+	$status = mysql_escape_string($r['status']);
+	$title = mysql_escape_string($r['desc']);
+	$message = mysql_escape_string($r['message']);
+	mysql_query("
+		INSERT INTO bugdb_resolves
+		SET name = '$key',
+			status = '$status',
+			title = '$title',
+			message = '$message',
+			project = 'php'
+	") or die (mysql_error());
 }
