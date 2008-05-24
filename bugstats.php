e6d37e07be5bcdb8070ab0b4c5c1bb11916444cb
Rasmus Lerdorf <rasmus@php.net>
2008-05-24 17:42:15 +0000

    Oops

--- bugstats.php
+++ bugstats.php
@@ -184,7 +184,7 @@ if ($phpver > 0) {
 $res = mysql_query($query);
 $row = mysql_fetch_assoc($res);
 
-$half = (int) $row['count']/2;
+$half = (int) ($row['count']/2);
 $query = "SELECT UNIX_TIMESTAMP(ts2)-UNIX_TIMESTAMP(ts1) AS half FROM bugdb WHERE ts2 > ts1";
 if ($phpver > 0) {
 	$query .= " AND SUBSTRING(php_version,1,1) = '$phpver'";
