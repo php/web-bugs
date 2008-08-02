0c30ce14b3260d9ab4906820fff45f2b3a643ceb
Rasmus Lerdorf <rasmus@php.net>
2008-08-02 16:14:26 +0000

    Fix 404 handler to work with Apache2

--- error.php
+++ error.php
@@ -1,6 +1,6 @@
 <?php
-$uri=substr($REDIRECT_ERROR_NOTES,strpos($REDIRECT_ERROR_NOTES,$DOCUMENT_ROOT)+strlen($DOCUMENT_ROOT)+1);
-
+#$uri=substr($REDIRECT_ERROR_NOTES,strpos($REDIRECT_ERROR_NOTES,$DOCUMENT_ROOT)+strlen($DOCUMENT_ROOT)+1);
+$uri = $_SERVER['REQUEST_URI'];
 if ($uri[0] == "/") $uri = substr($uri,1);
 Header("Location: http://bugs.php.net/bug.php?id=$uri");
 ?>
