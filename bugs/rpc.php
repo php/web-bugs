9f4adabb847018a1b96ea995a94ff54126e50175
Jani Taskinen <jani@php.net>
2009-07-21 13:47:16 +0000

    - Simplify, just use the TOKEN for authentication

--- bugs/rpc.php
+++ bugs/rpc.php
@@ -1,57 +1,49 @@
 <?php /* vim: set noet ts=4 sw=4: : */
 
-$id = (int)$_REQUEST['id'];
+$bug_id = (int) $_REQUEST['id'];
 
-if (!$id) {
-  echo json_encode(array('result' => array('error' => 'Missing bug id')));
-  exit;
+if (!$bug_id) {
+	echo json_encode(array('result' => array('error' => 'Missing bug id')));
+	exit;
 }
-      
+
 /**
  * Obtain common includes
  */
 require_once './include/prepend.inc';
-  
+
 // Authenticate
-bugs_authenticate($user, $pw, $logged_in, $is_trusted_developer);
-
-if (isset($_POST['MAGIC_COOKIE'])) {
-  list($user,$pw) = explode(":", base64_decode($_POST['MAGIC_COOKIE']), 2);
-} else {
-  echo json_encode(array('result' => array('error' => 'Missing credentials')));
-  exit;
-}
-
-if ($logged_in != 'developer') {
-  echo json_encode(array('result' => array('error' => 'Invalid user or password')));
-  exit;
+if ($_POST['token'] != md5(getenv('TOKEN'))) {
+	echo json_encode(array('result' => array('error' => 'Invalid token')));
+	exit;
 }
 
 # fetch info about the bug into $bug
 $bug = bugs_get_bug($bug_id);
 
 if (!is_array($bug)) {
-  echo json_encode(array('result' => array('error' => 'No such bug')));
-  exit;
+	echo json_encode(array('result' => array('error' => 'No such bug')));
+	exit;
 }
 
-if (!empty($_POST['ncomment'])) {
-    $ncomment = trim($_POST['ncomment']);
-    $res = $dbh->prepare('
-    	INSERT INTO bugdb_comments (bug, email, ts, comment, reporter_name, handle)
+if (!empty($_POST['ncomment']) && !empty($_POST['user'])) {
+	$user = htmlspecialchars(trim($_POST['user']));
+	$ncomment = htmlspecialchars(trim($_POST['ncomment']));
+	$res = $dbh->prepare('
+		INSERT INTO bugdb_comments (bug, email, ts, comment, reporter_name, handle)
 		VALUES (?, ?, NOW(), ?, ?, ?)
-	')->execute(array ($bug_id, $auth_user->email, $ncomment, $auth_user->name, $auth_user->handle));
+	')->execute(array ($bug_id, "{$user}@php.net", $ncomment, $user, $user));
 
-    if ($res) {
-        echo json_encode(array('result' => array('status' => $bug)));
-        exit;
-    } else {
-        echo json_encode(array('result' => array('error' => MDB2::errorMessage($res))));
-        exit;
-    }
+	if ($res) {
+		echo json_encode(array('result' => array('status' => $bug)));
+		exit;
+	} else {
+		echo json_encode(array('result' => array('error' => MDB2::errorMessage($res))));
+		exit;
+	}
 } else if (!empty($_POST['getbug'])) {
-    echo json_encode(array('result' => array('status' => $bug)));
-    exit;
+	echo json_encode(array('result' => array('status' => $bug)));
+	exit;
 }
 
 echo json_encode(array('result' => array('error' => 'Nothing to do')));
