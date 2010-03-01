606665b2da3ef5bae5975154c58dd847bb1f7dae
Jani Taskinen <jani@php.net>
2010-03-01 18:42:01 +0000

    - Update version list (need to really make this automatic)

--- include/php_versions.php
+++ include/php_versions.php
@@ -1,10 +1,11 @@
 <?php
 	$date = date('Y-m-d');
 	$versions = array(
-		'5.3.0',
+		'5.3.2RC3',
+		'5.3.1',
 		"5.3SVN-{$date} (snap)",
 		"5.3SVN-{$date} (SVN)",
-		'5.2.10',
+		'5.2.13',
 		"5.2SVN-{$date} (snap)",
 		"5.2SVN-{$date} (SVN)",
 		"6SVN-{$date} (snap)",
