894ffb83920c6fa68de7914b5ab9bee0bae114ac
Jani Taskinen <jani@php.net>
2010-03-01 18:33:30 +0000

    - Updated images, fixed paths

--- site/bugs-generating-backtrace-win32.php
+++ site/bugs-generating-backtrace-win32.php
@@ -57,31 +57,31 @@ first step is to configure the diagnostic tools. Select the
 tools menu and click on "Options and settings". The first tab contains
 the path to the symbols files, add the "debug folder" to the existing
 list using the "browse" button:</p>
-<p><img src="backtrace-images-win32/dbg_options.png" alt="Options"></p>
+<p><img src="/images/backtrace-images-win32/dbg_options.png" alt="Options"></p>
 
 <p>Now we are ready to generate our backtrace.</p>
 <p>We will use the wizard, click the "Add a rule" button and choose "Crash" as the rule type:</p>
-<p><img src="backtrace-images-win32/dbg_wizard_1.png" alt="Wizard #1"></p>
+<p><img src="/images/backtrace-images-win32/dbg_wizard_1.png" alt="Wizard #1"></p>
 
 <p>In the next window, select "a specific process":</p>
-<p><img src="backtrace-images-win32/dbg_wizard_2.png" alt="Wizard #2"></p>
+<p><img src="/images/backtrace-images-win32/dbg_wizard_2.png" alt="Wizard #2"></p>
 
 <p>Add a "sleep(10);" for the first run (from the cmd: "php.exe
 crashme.php"), it will let you enough time to click "next" and select
 the php process:</p>
-<p><img src="backtrace-images-win32/dbg_select_php.png" alt="Select the php process"></p>
+<p><img src="/images/backtrace-images-win32/dbg_select_php.png" alt="Select the php process"></p>
 
 <p>Click again next and let it crash. If everything went well, you should see your new rule as shown in the image below:</p>
-<p><img src="backtrace-images-win32/rules.jpg" alt="rules list"></p>
+<p><img src="/images/backtrace-images-win32/rules.jpg" alt="rules list"></p>
 
 <p>It also detected that "php.exe" was used. A rule has been created
 for all instance of "php.exe". It will save you the sleep and process
 selection.</p>
 <p>Now you can click the "Analyze data" button:</p>
-<p><img src="backtrace-images-win32/analyze.jpg" alt="Analyze"></p>
+<p><img src="/images/backtrace-images-win32/analyze.jpg" alt="Analyze"></p>
 
 <p>Et voila, the complete report will show up in your internet explorer (compressed html):</p>
-<p><img src="backtrace-images-win32/backtrace.jpg" alt="Debug report backtrace screenshot"></a></p>
+<p><img src="/images/backtrace-images-win32/backtrace.jpg" alt="Debug report backtrace screenshot"></a></p>
 
 <p>What we need is the backtrace itself which can be found under "Thread X - System ID XXX".</p>
 
