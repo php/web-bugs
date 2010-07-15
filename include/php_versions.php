5d5776e535245cc403dbce3d00f7a630d27f4825
Johannes Schlüter <johannes@php.net>
2010-07-15 11:16:53 +0000

    PHP 5.2.14RC3 and 5.3.3RC3

--- include/php_versions.php
+++ include/php_versions.php
@@ -1,11 +1,11 @@
 <?php
 	$date = date('Y-m-d');
 	$versions = array(
-		'5.3.3RC2',
+		'5.3.3RC3',
 		'5.3.2',
 		"5.3SVN-{$date} (snap)",
 		"5.3SVN-{$date} (SVN)",
-		'5.2.14RC2',
+		'5.2.14RC3',
 		'5.2.13',
 		"5.2SVN-{$date} (snap)",
 		"5.2SVN-{$date} (SVN)",
