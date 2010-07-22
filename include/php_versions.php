06a017a3cc9790755fa1c60ac4ac099ce65a6a4c
Johannes Schlüter <johannes@php.net>
2010-07-22 13:34:00 +0000

    - Website changes, except frontpage news entry, for PHP 5.2.14 and PHP 5.3.3

--- include/php_versions.php
+++ include/php_versions.php
@@ -1,12 +1,12 @@
 <?php
 	$date = date('Y-m-d');
 	$versions = array(
-		'5.3.3RC3',
-		'5.3.2',
+		//'5.3.3RC3',
+		'5.3.3',
 		"5.3SVN-{$date} (snap)",
 		"5.3SVN-{$date} (SVN)",
-		'5.2.14RC3',
-		'5.2.13',
+		//'5.2.14RC3',
+		'5.2.14',
 		"5.2SVN-{$date} (snap)",
 		"5.2SVN-{$date} (SVN)",
 		"trunk-SVN-{$date} (snap)",
