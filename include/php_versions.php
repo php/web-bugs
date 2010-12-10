fc2f08fcc04ef4e0243b1132c26b953b0d589f8a
Ilia Alshanetsky <iliaa@php.net>
2010-12-10 00:04:16 +0000

    5.2.15 and a bit of 5.3.4

--- include/php_versions.php
+++ include/php_versions.php
@@ -1,12 +1,10 @@
 <?php
 	$date = date('Y-m-d');
 	$versions = array(
-		'5.3.1RC2',
-		'5.3.3',
+		'5.3.4',
 		"5.3SVN-{$date} (snap)",
 		"5.3SVN-{$date} (SVN)",
-		'5.2.15RC2',
-		'5.2.14',
+		'5.2.15',
 		"5.2SVN-{$date} (snap)",
 		"5.2SVN-{$date} (SVN)",
 		"trunk-SVN-{$date} (snap)",
