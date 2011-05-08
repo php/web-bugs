36565923054f2e788e4b6f800fa2a5459243cf6f
Philip Olson <philip@php.net>
2011-05-08 20:12:57 +0000

    Added simple API, with its only [current] use being for the doc team, regarding who closed bugs last week.

---
+++ www/api.php
@@ -0,0 +1,42 @@
+<?php
+/*
+	Hack note: This is for emailing the documentation team about commit/bugs emails, but could be expanded in the future.
+
+	The API itself will probably be abandoned in the future, but here's the current URL:
+	- http://bugs.php.net/api.php?type=docs&action=closed&interval=week
+*/
+require_once '../include/prepend.php';
+
+$type     = isset($_GET['type'])     ? $_GET['type']           : 'unknown';
+$action   = isset($_GET['action'])   ? $_GET['action']         : 'unknown';
+$interval = isset($_GET['interval']) ? $_GET['interval']       : 'unknown';
+
+if ($type === 'docs' && $action === 'closed' && $interval === 'week') {
+
+	$query = 
+	"
+		SELECT bugdb_comments.reporter_name, COUNT(*) as count
+		FROM bugdb_comments, bugdb 
+		WHERE comment_type =  'log' 
+		AND package_name IN ('Doc Build problem', 'Documentation problem', 'Translation problem', 'Online Doc Editor problem') 
+		AND comment LIKE  '%+Status:      Closed</span>%'
+		AND date_sub(curdate(), INTERVAL 7 DAY) <= ts
+		AND bugdb.id = bugdb_comments.bug
+		GROUP BY bugdb_comments.reporter_name
+		ORDER BY count DESC
+	";
+	
+	//@todo add error handling
+	$rows = $dbh->prepare($query)->execute(array())->fetchAll(MDB2_FETCHMODE_ASSOC);
+	if (!$rows) {
+		echo 'The fail train has arrived.';
+		exit;
+	}
+	
+	echo serialize($rows);
+
+} else {
+	
+	echo "Unknown action";
+	
+}
