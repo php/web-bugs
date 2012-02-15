15bbd610d06bac00ce8f5eee07e44f6b609211ae
Rasmus Lerdorf <rasmus@php.net>
2012-02-15 14:28:50 -0800

    Annoying link spammer

--- include/functions.php
+++ include/functions.php
@@ -252,7 +252,7 @@ function is_spam($string)
 	if (substr_count(strtolower($string), 'http://') > 5) {
 		return true;
 	}
-	if (preg_match("/(asian)|(spy)|(bdsm)|(massage)|(mortage)|(sex)(?<!OutOfBoundsEx(?=ception))|(11nong)|(oxycontin)|(distance-education)|(sismatech)|(justiceplan)|(prednisolone)|(baclofen)|(diflucan)|(unbra.se)|(objectis)/i", $string)) {
+	if (preg_match("/(asian)|(spy)|(bdsm)|(massage)|(mortage)|(sex)(?<!OutOfBoundsEx(?=ception))|(11nong)|(oxycontin)|(distance-education)|(sismatech)|(justiceplan)|(prednisolone)|(baclofen)|(diflucan)|(unbra.se)|(objectis)|(angosso)/i", $string)) {
 		return true;
 	}
 	if (preg_match("~/Members/~", $string)) {
