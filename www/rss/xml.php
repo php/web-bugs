e9b937bb165a27b04149d9a8f0921fb3441afd05
Christoph M. Becker <cmbecker69@gmx.de>
2018-07-24 14:55:38 +0200

    Fix rss/bug.php?format=xml
    Although, `MDB2_FETCHMODE_ASSOC` is supposed to return an associative
    array (mapping column names to their values)[1], for some reason it
    actually seems to behave like some non-existing `MDB2_FETCHMODE_BOTH`.
    This breaks the generated XML by additionally creating invalid element
    names.  Since we are not sure whether other callers of bugs_get_bug()
    and bugs_get_bug_comments() rely on this behavior, we're working around
    the issue by skipping the integer keys.
    
    [1] <http://pear.php.net/manual/en/package.database.mdb2.intro-fetch.php#package.database.mdb2.intro-fetch.desc.format>

--- www/rss/xml.php
+++ www/rss/xml.php
@@ -1,12 +1,14 @@
 <?php
 echo "<bug>\n";
 foreach ($bug as $key => $value) {
+	if (is_int($key)) continue;
 	echo "  <$key>", clean($value), "</$key>\n";
 }
 foreach ($comments as $comment) {
 	if (empty($comment['registered'])) continue;
 	echo "  <comment>\n";
 	foreach ($comment as $key => $value) {
+		if (is_int($key)) continue;
 		echo "	<$key>", clean($value), "</$key>\n";
 	}
 	echo "  </comment>\n";
