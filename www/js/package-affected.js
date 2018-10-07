96224c7a8033593820a1a4d28970022044864313
zrhoffman <zrhoffman@ku.edu>
2018-10-07 15:42:23 -0500

    package-affected.js: change the package list when the user selects a different package group

--- www/js/package-affected.js
+++ www/js/package-affected.js
@@ -7,7 +7,10 @@ window.addEventListener(
 			.querySelectorAll('select[name="in[package_name]"]')
 			.forEach(
 				function (select) {
-					var packageGroup = document.createElement('select');
+					var packageGroup = document.createElement('select'),
+						initialValue = select.value,
+						initialGroup = select.querySelector(':scope option[value="' + initialValue + '"]').parentNode;
+
 					packageGroup.name = 'in[package_group]';
 
 					select
@@ -20,8 +23,69 @@ window.addEventListener(
 								packageOption.textContent = groupName;
 								packageOption.value = groupName;
 								packageGroup.appendChild(packageOption);
+
+								optgroup
+									.querySelectorAll(':scope option')
+									.forEach(
+										function (option) {
+											option.setAttribute('bug-group', groupName);
+										}
+									);
 							}
 						);
+
+					var instructions = select.querySelector(':scope option[value="none"]');
+					if (instructions instanceof HTMLElement) {
+						instructions.textContent = 'Select a category';
+					}
+
+					select
+						.querySelectorAll(':scope optgroup')
+						.forEach(
+							function (optgroup) {
+								optgroup.style.display = 'none';
+							}
+						);
+
+					function updateGroup() {
+						select.disabled = false;
+
+						if (instructions instanceof HTMLElement) {
+							instructions.style.display = 'none';
+						}
+
+						var previousOptions = select.querySelectorAll(':scope > option:not([value=none])'),
+							nextLabel = packageGroup.value,
+							nextGroup = select.querySelector(':scope optgroup[label="' + nextLabel + '"]');
+
+						if (previousOptions.length !== 0) {
+							var previousLabel = previousOptions[0].getAttribute('bug-group'),
+								previousGroup = select.querySelector(':scope optgroup[label="' + previousLabel + '"]');
+
+							moveOptions(select, previousGroup);
+						}
+
+						moveOptions(nextGroup, select);
+					}
+
+					function moveOptions(from, to) {
+						from.querySelectorAll(':scope > option')
+							.forEach(
+								function (option) {
+									to.appendChild(option);
+								}
+							);
+					}
+
+					packageGroup.addEventListener('change', updateGroup);
+
+					if (initialGroup instanceof HTMLOptGroupElement) {
+						packageGroup.value = initialGroup.label;
+						moveOptions(initialGroup, select);
+					} else {
+						select.disabled = true;
+						select.value = null;
+					}
 
 					select
 						.parentNode
