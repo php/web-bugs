0094957712fdbc877f251577be17e9b87a9bdca6
zrhoffman <zrhoffman@ku.edu>
2018-10-07 15:16:58 -0500

    package-affected.js: a dropdown for package categories

--- www/js/package-affected.js
+++ www/js/package-affected.js
@@ -1,1 +1,32 @@
 'use strict';
+
+window.addEventListener(
+	'load',
+	function () {
+		document
+			.querySelectorAll('select[name="in[package_name]"]')
+			.forEach(
+				function (select) {
+					var packageGroup = document.createElement('select');
+					packageGroup.name = 'in[package_group]';
+
+					select
+						.querySelectorAll(':scope optgroup')
+						.forEach(
+							function (optgroup) {
+								var packageOption = document.createElement('option'),
+									groupName = optgroup.label;
+
+								packageOption.textContent = groupName;
+								packageOption.value = groupName;
+								packageGroup.appendChild(packageOption);
+							}
+						);
+
+					select
+						.parentNode
+						.insertBefore(packageGroup, select);
+				}
+			);
+	}
+);
