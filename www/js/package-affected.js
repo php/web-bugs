5344b77166b0826165792f12042a72383fcbba93
zrhoffman <zrhoffman@ku.edu>
2018-10-07 14:48:20 -0500

    Fix #76273: Package affected section should be split
    - include package-affected.js on bug reporting page
    - package-affected.js: a dropdown for package categories
    - package-affected.js: change the package list when the user selects a different package group
    - packages affected: change select size to 5 so the user can see when the the package list changes

---
+++ www/js/package-affected.js
@@ -0,0 +1,104 @@
+'use strict';
+
+window.addEventListener(
+	'load',
+	function () {
+		document
+			.querySelectorAll('select[name="in[package_name]"]')
+			.forEach(
+				function (select) {
+					var packageGroup = document.createElement('select'),
+						initialValue = select.value,
+						initialGroup = select.querySelector(':scope option[value="' + initialValue + '"]').parentNode;
+
+					packageGroup.name = 'in[package_group]';
+
+					select
+						.querySelectorAll(':scope optgroup')
+						.forEach(
+							function (optgroup) {
+								var packageOption = document.createElement('option'),
+									groupName = optgroup.label;
+
+								packageOption.textContent = groupName;
+								packageOption.value = groupName;
+								packageGroup.appendChild(packageOption);
+
+								optgroup
+									.querySelectorAll(':scope option')
+									.forEach(
+										function (option) {
+											option.setAttribute('bug-group', groupName);
+										}
+									);
+							}
+						);
+
+					var instructions = select.querySelector(':scope option[value="none"]');
+					if (instructions instanceof HTMLElement) {
+						instructions.textContent = 'Select a category';
+					}
+
+					select
+						.querySelectorAll(':scope optgroup')
+						.forEach(
+							function (optgroup) {
+								optgroup.style.display = 'none';
+							}
+						);
+
+					function updateGroup() {
+						select.disabled = false;
+
+						if (instructions instanceof HTMLElement) {
+							instructions.style.display = 'none';
+						}
+
+						var previousOptions = select.querySelectorAll(':scope > option:not([value=none])'),
+							nextLabel = packageGroup.value,
+							nextGroup = select.querySelector(':scope optgroup[label="' + nextLabel + '"]');
+
+						if (previousOptions.length !== 0) {
+							var previousLabel = previousOptions[0].getAttribute('bug-group'),
+								previousGroup = select.querySelector(':scope optgroup[label="' + previousLabel + '"]');
+
+							moveOptions(select, previousGroup);
+						}
+
+						moveOptions(nextGroup, select);
+					}
+
+					function moveOptions(from, to) {
+						from.querySelectorAll(':scope > option')
+							.forEach(
+								function (option) {
+									to.appendChild(option);
+								}
+							);
+					}
+
+					packageGroup.addEventListener('change', updateGroup);
+
+					if (initialGroup instanceof HTMLOptGroupElement) {
+						packageGroup.value = initialGroup.label;
+						moveOptions(initialGroup, select);
+					} else {
+						select.disabled = true;
+						select.value = null;
+					}
+
+					packageGroup.style.marginRight = '.5em';
+					[select, packageGroup].forEach(
+						function (element) {
+							element.size = 5;
+							element.style.width = '22em';
+						}
+					);
+
+					select
+						.parentNode
+						.insertBefore(packageGroup, select);
+				}
+			);
+	}
+);
