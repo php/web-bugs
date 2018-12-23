e11947ebd1e70be448cb3e3591d626c906e0d8d0
Peter Kokot <peterkokot@gmail.com>
2018-12-23 19:29:07 +0100

    Use location instead of window.location

--- www/js/redirect.js
+++ www/js/redirect.js
@@ -4,14 +4,13 @@
  * Servers can't deal properly with URLs containing hash. This redirects bug id
  * passed as #id to the front controller. For example,
  * https://bugs.php.net/#12345
- *
  */
 
 window.addEventListener('load', function () {
-    var bugId = window.location.hash.substr(1) * 1;
+    var bugId = location.hash.substr(1) * 1;
 
     if (bugId > 0) {
-        var loc = window.location;
+        var loc = location;
         loc.href = loc.protocol + '//' + loc.host + (loc.port ? ':' + loc.port : '') + '/' + bugId;
     }
 });
