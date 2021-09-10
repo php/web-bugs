ad291d2958536bbc81069fb513bc15a82d06b9cd
peterdd <ddpm@liscovius.de>
2021-09-10 13:19:39 +0200

    Update index.php

--- templates/pages/index.php
+++ templates/pages/index.php
@@ -9,7 +9,7 @@ steps:</p>
 
 <ul>
     <li>
-        Used the form above or our <a href="/search.php">advanced search page</a>
+        Used the form above or our <a href="search.php">advanced search page</a>
         to make sure nobody has reported the bug already.
     </li>
 
@@ -19,7 +19,7 @@ steps:</p>
     </li>
 
     <li>
-        Read our tips on <a href="/how-to-report.php">how to report a bug that
+        Read our tips on <a href="how-to-report.php">how to report a bug that
         someone will want to help fix</a>.
     </li>
 
@@ -30,8 +30,8 @@ steps:</p>
 
     <li>
         See how to get a backtrace in case of a crash:
-        <a href="/bugs-generating-backtrace.php">for *NIX</a> and
-        <a href="/bugs-generating-backtrace-win32.php">for Windows</a>.
+        <a href="bugs-generating-backtrace.php">for *NIX</a> and
+        <a href="bugs-generating-backtrace-win32.php">for Windows</a>.
     </li>
 
     <li>
@@ -42,19 +42,19 @@ steps:</p>
 
 <p>Once you've double-checked that the bug you've found hasn't already been
 reported, and that you have collected all the information you need to file an
-excellent bug report, you can do so on our <a href="/report.php">bug reporting
+excellent bug report, you can do so on our <a href="report.php">bug reporting
 page</a>.</p>
 
 <h1>Search the Bug System</h1>
 
 <p>You can search all of the bugs that have been reported on our
-<a href="/search.php">advanced search page</a>, or use the form at the top of the
-page for a basic default search. Read the <a href="/search-howto.php">search howto</a>
+<a href="search.php">advanced search page</a>, or use the form at the top of the
+page for a basic default search. Read the <a href="search-howto.php">search howto</a>
 for instructions on how search works.</p>
 
 <p>If you have 10 minutes to kill and you want to help us out, grab a random
 open bug and see if you can help resolve it. We have made it easy. Hit
-<a href="/random">random</a> to go directly to a random open bug.</p>
+<a href="random">random</a> to go directly to a random open bug.</p>
 
 <p>Common searches</p>
 
@@ -67,10 +67,10 @@ open bug and see if you can help resolve it. We have made it easy. Hit
 <h1>Bug System Statistics</h1>
 
 <p>You can view a variety of statistics about the bugs that have been reported
-on our <a href="/stats.php">bug statistics page</a>.</p>
+on our <a href="stats.php">bug statistics page</a>.</p>
 
 <?php $this->end('content') ?>
 
 <?php $this->start('scripts') ?>
-    <script src="/js/redirect.js"></script>
+    <script src="js/redirect.js"></script>
 <?php $this->end('scripts') ?>
