c2a59a52fa60c5abc8e3b9c573e3a1d63a16b4c9
peterdd <ddpm@liscovius.de>
2021-09-18 22:14:21 +0200

    fix typo in tests/bootstrap.php
    I had to fix the path there to enable running the phpunit tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,6 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-define('TEST_FIXTURES_DIRECTORY', __DIR__ . '/Fixtures');
+define('TEST_FIXTURES_DIRECTORY', __DIR__ . '/fixtures');
 define('TEST_MOCKS_DIRECTORY', __DIR__ . '/Mocks');
 define('TEST_VAR_DIRECTORY', __DIR__ . '/../var');
