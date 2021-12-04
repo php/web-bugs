3fb5cb12012a3d6500846903a5790ef9190781fb
Nikita Popov <nikita.ppv@gmail.com>
2021-12-04 21:07:31 +0100

    Hide bug submission form by default
    As people will not read anything no matter how red or bold it is,
    hide the bug submission form by default. We don't want people to
    submit their mundane bugs as security issues just because that's
    the only option.

--- www/report.php
+++ www/report.php
@@ -363,17 +363,20 @@ if (!isset($_POST['in'])) {
 ?>
 
     <p style="background-color: #faa;">
-        <strong>This bug tracker no longer accepts new non-security issues. Instead use one of the following.</strong>
-        <ul style="background-color: #faa">
+        <strong>This bug tracker no longer accepts new non-security issues. Instead use one of the following:</strong>
+        <ul>
             <li>Implementation issues: <a href="https://github.com/php/php-src/issues">php/php-src repository</a></li>
             <li>Documentation issues: <a href="https://github.com/php/doc-en/issues">php/doc-en repository</a></li>
             <li>PECL extension issues: Find the correct extension-specific bug tracker at <a href="https://pecl.php.net/">pecl.php.net</a></li>
             <li>PEAR issues: <a href="https://pear.php.net/bugs/">pear.php.net/bugs</a></li>
-            <li>Security issues: Use the form below, or email <?php echo make_mailto_link("{$site_data['security_email']}?subject=%5BSECURITY%5D+possible+new+bug%21", $site_data['security_email']); ?></li>
+            <li>Security issues: Proceed with <a href="report.php?security_bug">this form</a>, or email <?php echo make_mailto_link("{$site_data['security_email']}?subject=%5BSECURITY%5D+possible+new+bug%21", $site_data['security_email']); ?></li>
         </ul>
     </p>
 
 <?php
+    if (!isset($_GET['security_bug'])) {
+        return;
+    }
 
 }
 
